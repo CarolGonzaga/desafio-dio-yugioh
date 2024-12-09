36c44fe07ded7df5a92b2e70f8290ff085abdde5
CarolGonzaga <dev.carolgonzaga@gmail.com>
2024-12-09 15:27:48 -0300

    personalizacao do projeto

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,11 +1,12 @@
+// Define o estado padrão do jogo
 const state = {
-    score: {
+    score: { 
         playerScore: 0,
         computerScore: 0,
         scoreBox: document.getElementById('score_points'),
     },
     
-    cardSprites: {
+    cardSprites: { 
         avatar: document.getElementById('card-img'),
         name: document.getElementById('card-name'),
         type: document.getElementById('card-type'),
@@ -16,7 +17,7 @@ const state = {
         computer: document.getElementById('computer-field-card'),
     },
 
-    playerSide: {
+    playerSide: { 
         player1: "player-cards",
         player1Box: document.querySelector("#player-cards"),
         computer: "computer-cards",
@@ -28,44 +29,127 @@ const state = {
     },
 };
 
+// Caminho para os arquivos de imagens das cartas
 const pathImages = "./src/assets/icons/";
 
+// Tipos de Cartas
+const cardTypes = {
+    ROCK: "Pedra",
+    PAPER: "Papel",
+    SCISSORS: "Tesoura",
+};
+
+// Dados das Cartas
 const cardData = [
     {
         id: 0,
-        name: "Dragão Branco de Olhos Azuis",
-        type: "Papel",
-        img: `${pathImages}dragon.png`,
-        winOf: [1],
-        loseOf: [2],
+        name: "Cilindro Mágico",
+        type: cardTypes.SCISSORS,
+        img: `${pathImages}cilindro.png`,
+        winOf: cardTypes.PAPER,
+        loseOf: cardTypes.ROCK,
     },
     {
         id: 1,
-        name: "Mago Negro",
-        type: "Pedra",
-        img: `${pathImages}magician.png`,
-        winOf: [2],
-        loseOf: [0],
+        name: "Dragão Alado de Rá",
+        type: cardTypes.SCISSORS,
+        img: `${pathImages}dragao-alado.png`,
+        winOf: cardTypes.PAPER,
+        loseOf: cardTypes.ROCK,
     },
     {
         id: 2,
+        name: "Dragão Branco de Olhos Azuis",
+        type: cardTypes.ROCK,
+        img: `${pathImages}dragao-branco.png`,
+        winOf: cardTypes.SCISSORS,
+        loseOf: cardTypes.PAPER,
+    },
+    {
+        id: 3,
+        name: "Dragão Negro",
+        type: cardTypes.ROCK,
+        img: `${pathImages}dragao-negro.png`,
+        winOf: cardTypes.SCISSORS,
+        loseOf: cardTypes.PAPER,
+    },
+    {
+        id: 4,
+        name: "Slifer, o Dragão Celeste",
+        type: cardTypes.SCISSORS,
+        img: `${pathImages}dragao-slifer.png`,
+        winOf: cardTypes.PAPER,
+        loseOf: cardTypes.ROCK,
+    },
+    {
+        id: 5,
+        name: "Espadas da Luz Reveladora",
+        type: cardTypes.PAPER,
+        img: `${pathImages}espadas.png`,
+        winOf: cardTypes.ROCK,
+        loseOf: cardTypes.SCISSORS,
+    },
+    {
+        id: 6,
         name: "Exodia",
-        type: "Tesoura",
+        type: cardTypes.SCISSORS,
         img: `${pathImages}exodia.png`,
-        winOf: [0],
-        loseOf: [1],
+        winOf: cardTypes.PAPER,
+        loseOf: cardTypes.ROCK,
+    },
+    {
+        id: 7,
+        name: "Mago Do Tempo",
+        type: cardTypes.PAPER,
+        img: `${pathImages}mago-do-tempo.png`,
+        winOf: cardTypes.ROCK,
+        loseOf: cardTypes.SCISSORS,
+    },
+    {
+        id: 8,
+        name: "Mago Negro",
+        type: cardTypes.ROCK,
+        img: `${pathImages}mago-negro.png`,
+        winOf: cardTypes.SCISSORS,
+        loseOf: cardTypes.PAPER,
+    },
+    {
+        id: 9,
+        name: "Obelisco, o Atormentador",
+        type: cardTypes.PAPER,
+        img: `${pathImages}obelisco.png`,
+        winOf: cardTypes.ROCK,
+        loseOf: cardTypes.SCISSORS,
+    },
+    {
+        id: 10,
+        name: "Caveira Invocada",
+        type: cardTypes.PAPER,
+        img: `${pathImages}caveira.png`,
+        winOf: cardTypes.ROCK,
+        loseOf: cardTypes.SCISSORS,
+    },
+    {
+        id: 11,
+        name: "Monstro Renasce",
+        type: cardTypes.ROCK,
+        img: `${pathImages}renascer.png`,
+        winOf: cardTypes.SCISSORS,
+        loseOf: cardTypes.PAPER,
     },
 ];
 
+// Retorna o ID de uma carta aleatória
 async function getRandomCardId() {
     const randomIndex = Math.floor(Math.random() * cardData.length);
     return cardData[randomIndex].id;
 }
 
+// Cria um elemento de imagem para a carta
 async function createCardImage(idCard, fieldSide) {
     const cardImage = document.createElement('img');
     cardImage.classList.add("card");
-    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.jpg");
     cardImage.setAttribute("data-id", idCard);
 
     if (fieldSide === state.playerSide.player1) {
@@ -81,23 +165,26 @@ async function createCardImage(idCard, fieldSide) {
     return cardImage;
 }
 
+// Distribue as cartas
 async function drawCards(cardNumbers, fieldSide) {
     for (let index = 0; index < cardNumbers; index++) {
         const randomIdCard = await getRandomCardId();
         const cardImage = await createCardImage(randomIdCard, fieldSide);
         
-        document.getElementById(fieldSide).appendChild(cardImage)
+        document.getElementById(fieldSide).appendChild(cardImage);
     }
 }
 
+// Exibe detalhes da carta selecionada
 async function drawSelectCard(index) {
     state.cardSprites.avatar.src = cardData[index].img;
     state.cardSprites.name.innerText = cardData[index].name;
     state.cardSprites.type.innerText = `Atributo: ${cardData[index].type}`;
 }
 
+// Remove todas as cartas atualmente visíveis
 async function removeAllCardsImage() {
-    let {computerBox, player1Box} = state.playerSide;
+    let { computerBox, player1Box } = state.playerSide;
     let imgElements = computerBox.querySelectorAll("img");
     imgElements.forEach((img) => img.remove());
 
@@ -105,40 +192,56 @@ async function removeAllCardsImage() {
     imgElements.forEach((img) => img.remove());
 }
 
+// Checa o resultado do duelo
 async function checkDuelResults(playerCardId, computerCardId) {
+    
     let duelResults = "empate";
-    let playerCard= cardData[playerCardId];
+    const playerCard = cardData[playerCardId];
+    const computerCard = cardData[computerCardId];
 
-    if (playerCard.winOf.includes(computerCardId)) {
+    // Lógica para determinar o vencedor
+    if (
+        (playerCard.type === cardTypes.ROCK && computerCard.type === cardTypes.SCISSORS) ||
+        (playerCard.type === cardTypes.PAPER && computerCard.type === cardTypes.ROCK) ||
+        (playerCard.type === cardTypes.SCISSORS && computerCard.type === cardTypes.PAPER)
+    ) {
         duelResults = "ganhou";
         state.score.playerScore++;
-    }
-
-    if (playerCard.loseOf.includes(computerCardId)) {
+    
+    } else if (
+        (computerCard.type === cardTypes.ROCK && playerCard.type === cardTypes.SCISSORS) ||
+        (computerCard.type === cardTypes.PAPER && playerCard.type === cardTypes.ROCK) ||
+        (computerCard.type === cardTypes.SCISSORS && playerCard.type === cardTypes.PAPER)
+    ) {
         duelResults = "perdeu";
         state.score.computerScore++;
     }
 
+    // Toca o áudio correspondente ao resultado
     await playAdio(duelResults);
 
     return duelResults;
 }
 
+// Atualiza o texto do botão de ação
 async function drawButton(text) {
     state.actions.button.innerText = text;
     state.actions.button.style.display = "block";
 }
 
+// Atualiza o placar na interface
 async function updateScore() {
-    state.score.scoreBox.innerText = `Win : ${state.score.playerScore} | Lose: ${state.score.computerScore}`
+    state.score.scoreBox.innerText = `Win : ${state.score.playerScore} | Lose: ${state.score.computerScore}`;
 }
 
+// Oculta os detalhes da carta
 async function hiddenCardDetails() {
     state.cardSprites.avatar.src = "";
     state.cardSprites.name.innerText = "";
     state.cardSprites.type.innerText = "";
 }
 
+// Mostra ou esconde as cartas de cada jogador
 async function showHiddenCardFieldsImage(value) {
     if (value) {
         state.fieldCards.player.style.display = "block";
@@ -149,11 +252,13 @@ async function showHiddenCardFieldsImage(value) {
     }
 }
 
+// Exibe as cartas no campo de duelo
 async function drawCardsInField(cardId, computerCardId) {
     state.fieldCards.player.src = cardData[cardId].img;
     state.fieldCards.computer.src = cardData[computerCardId].img;
 }
 
+// Configura o campo de duelo e processa o resultado
 async function setCardsField(cardId) {
     await removeAllCardsImage();
     await hiddenCardDetails();
@@ -169,6 +274,7 @@ async function setCardsField(cardId) {
     await drawButton(duelResults);
 }
 
+// Reinicia o duelo
 async function resetDuel() {
     state.cardSprites.name.innerText = "Escolha uma nova Carta";
     state.actions.button.style.display = "none";
@@ -178,11 +284,13 @@ async function resetDuel() {
     init();
 }
 
+// Toca o áudio conforme o resultado do duelo
 async function playAdio(status) {
     const audio = new Audio(`./src/assets/audios/${status}.wav`);
     audio.play();
 }
 
+// Inicializa o jogo
 function init() {
     drawCards(5, state.playerSide.player1);
     drawCards(5, state.playerSide.computer);
